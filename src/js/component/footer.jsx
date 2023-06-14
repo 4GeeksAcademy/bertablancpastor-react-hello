0d395954ba354f8f913e87911a778b97fcd12c55
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-06-14 11:00:11 +0000

    finalizado proyecto react y bootstrap

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Component Footer
+
+const Footer = () => {
+    return (
+        <footer className="bg-light text-center text-lg-start bg-secondary">
+            <div className="text-center p-3 bg-secondary text-white">
+            Copyright © 
+            <a className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="https://github.com/bertablancpastor"> My GitHub 2023 </a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Jumbotron component
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron container bg-light pb-5 ps-4">
+            <h1 className="display-4">A Warm Welcome!</h1>
+            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolores commodi, sunt a consequuntur deleniti, neque odio officiis incidunt iusto, rerum animi corporis voluptate corrupti ea error accusantium. Tempora, et!</p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+        </div>
+
+    );
+};
+
+export default Jumbotron
